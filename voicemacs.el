b6b9ec59e5b2de36b4e5e9fb16929a32f8a5080a
jcaw <40725916+jcaw@users.noreply.github.com>
2020-02-05 16:54:12 +0000

    Initial commit - add skeleton minor mode for voice programming
    When active, will provide various utilities/services that are useful for a voice
    recognition system.

---
+++ voicemacs.el
@@ -0,0 +1,31 @@
+(defgroup voicemacs nil
+  "Utilities to make Emacs easier to control by voice."
+  :prefix "voicemacs-")
+
+
+(defun voicemacs--mode-disable ()
+  ;; TODO: Disable title modification
+  ;; TODO: Stop syncer
+  ;; TODO: Stop server
+  )
+
+
+(defun voicemacs--mode-enable ()
+  ;; TODO: Start server
+  ;; TODO: Start syncer
+  ;; TODO: Enable title modification
+  )
+
+
+(define-minor-mode voicemacs-mode
+  "Minor mode to communicate with voice recognition software."
+  :group 'voicemacs
+  :global t
+  :init-value t
+  :lighter nil
+  :after-hook (if voicemacs-mode
+                  (voicemacs--mode-enable)
+                (voicemacs--mode-disable)))
+
+
+(provide 'voicemacs)
