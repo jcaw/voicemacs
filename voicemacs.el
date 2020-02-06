dff94c768a5aa443c2e72844b6d3bbd39c179c49
jcaw <40725916+jcaw@users.noreply.github.com>
2020-02-06 13:30:56 +0000

    Modify title to display a data string while voicemacs is active
    We can use this to communicate information to local voice systems without having
    a direct connection open. This is really a hack until we have some kind of
    persistent socket, which is probably going to be faster and more reliable.

--- voicemacs.el
+++ voicemacs.el
@@ -1,10 +1,56 @@
+(require 'json)
+
+
 (defgroup voicemacs nil
   "Utilities to make Emacs easier to control by voice."
   :prefix "voicemacs-")
 
 
+(defvar voicemacs--title-data ""
+  "JSON-formatted string containing data that is appended to the title.
+
+Used to communicate information to external engines without a
+direct connection.")
+
+
+(defconst voicemacs--title-suffix
+  '(:eval (concat " ; " voicemacs--title-data))
+  "The title format used by voicemacs.")
+
+
+(defvar voicemacs--old-title-format nil
+  "The title format before `voicemacs-mode' was enabled.")
+
+
+(defun voicemacs--voicemacs-title-p ()
+  "Is the title currently in voicemacs format?"
+  (let ((default-title (default-value 'frame-title-format)))
+    (and (listp default-title)
+         (member voicemacs--title-suffix default-title)
+         ;; Cast to bool
+         t)))
+
+
+(defun voicemacs--set-title ()
+  "Set the title to the voicemacs format."
+  (unless (voicemacs--voicemacs-title-p)
+    (setq voicemacs--old-title-format (default-value 'frame-title-format))
+    (setq-default frame-title-format (list frame-title-format
+                                           voicemacs--title-suffix))))
+
+
+(defun voicemacs--restore-title ()
+  "Restore the original title format.
+
+Note this will only revert a voicemacs-formatted title. If thet
+title format has been modified since, this method will leave the
+new format."
+  (setq-default frame-title-format voicemacs--old-title-format)
+  (setq voicemacs--old-title-format nil))
+
+
 (defun voicemacs--mode-disable ()
-  ;; TODO: Disable title modification
+  (voicemacs--restore-title)
   ;; TODO: Stop syncer
   ;; TODO: Stop server
   )
@@ -13,8 +59,7 @@
 (defun voicemacs--mode-enable ()
   ;; TODO: Start server
   ;; TODO: Start syncer
-  ;; TODO: Enable title modification
-  )
+  (voicemacs--set-title))
 
 
 (define-minor-mode voicemacs-mode
