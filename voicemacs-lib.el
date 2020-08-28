946eb1ae7a0b2dbcf1c4ea455c468e2c274af926
jcaw <toastedjcaw@gmail.com>
2020-08-28 12:20:16 +0100

    Add miscellaneous lib methods

--- voicemacs-lib.el
+++ voicemacs-lib.el
@@ -3,6 +3,9 @@
 ;;; Code:
 
 
+(require 'nav-flash)
+
+
 (defun voicemacs--pad-string (string desired-length)
   "Pad left side of `STRING' to `DESIRED-LENGTH'."
   (concat (make-string
@@ -19,6 +22,49 @@
 Like `bound-and-true-p', but a function, not a macro."
   (and (boundp symbol)
        (symbol-value symbol)))
+
+
+(defun voicemacs-in-string-p ()
+  "Returns t if currently in a string, nil otherwise.
+
+`in-string-p' is obsolete so use this instead."
+  (if (nth 3 (syntax-ppss)) t nil))
+
+
+(defun voicemacs-assert-in (value valid-values)
+  "Ensure a `VALUE' is one of `VALID-VALUES', with meaningful error."
+  (unless (member value valid-values)
+    (user-error "%s not one of %s" value valid-values)))
+
+
+(defun voicemacs-flash (region-cons)
+  "Flash a region in the buffer."
+  (when region-cons
+    ;; TODO: Don't use `nav-flash' for this. It doesn't block.
+    (nav-flash-show (car region-cons) (cdr region-cons) 'isearch)))
+
+
+(defun voicemacs-visual-copy (region-cons)
+  (voicemacs-flash region-cons)
+  (kill-ring-save (car region-cons) (cdr region-cons)))
+
+
+(defun voicemacs-visual-kill (region-cons)
+  (voicemacs-flash region-cons)
+  (kill-region (car region-cons) (cdr region-cons)))
+
+
+(defun voicemacs-visual-yank ()
+  (let ((start-pos (point)))
+    (yank)
+    (voicemacs-flash (cons start-pos (point)))))
+
+
+;; TODO: Smart insert, respecting e.g. whitespace.
+(defun voicemacs-visual-insert (text)
+  (let ((start-pos (point)))
+    (insert text)
+    (voicemacs-flash (cons start-pos (point)))))1
 
 
 (defmacro voicemacs--first-result (result-form &rest body)
@@ -40,6 +86,15 @@ Like with `mapatoms', `OBARRAY-' defaults to the value of
                   (push atom matches)))
               (or obarray- obarray))
     matches))
+
+
+(defun voicemacs--maphash (func table)
+  "Like `maphash', but returns the list of results like `mapcar'."
+  (let ((results))
+    (maphash (lambda (key value)
+               (push (funcall func key value) results))
+             table)
+    results))
 
 
 (provide 'voicemacs-lib)
